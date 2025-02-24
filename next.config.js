0418bf43b02eb08bb4f83302bf205d1650c1897b
Hasan Barış <hasan.baris.s@hotmail.com>
2025-02-24 16:27:44 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+// next.config.js
+module.exports = {
+  // config options here
+  reactStrictMode: true, // Örnek bir yapılandırma
+}
